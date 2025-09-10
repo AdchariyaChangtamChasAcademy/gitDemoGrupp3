8e1a1456a65c870a1f3c2e322c2001e994e67916
Adchariya Changtam <adchariya.changtam@chasacademy.se>
2025-09-10 11:00:30 +0200

    isEven metoden is now finished.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,49 @@
-﻿namespace gitDemoGrupp3
+﻿using System.Diagnostics.Metrics;
+
+namespace gitDemoGrupp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while(true)
+            {
+                Console.Write("Siffra att kolla: ");
+                int inputNumber = int.Parse(Console.ReadLine());
+
+                if (inputNumber == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    isEven(inputNumber);
+                }
+            }
+        }
+
+        static bool isEven(int number)
+        {
+            //X []  Skapa en metod som ska heta “IsEven” och returnerar följande:
+            //- []  `True` om talet är jämnt, `False` annars.
+            //- []  Använd en loop i `Main` metoden för att låta användaren testa flera tal tills de skriver in `0`
+            //- []  Om användaren skriver `0` avslutas programmet.
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("true");
+                return true;
+            }
+            else if (number % 2 != 0)
+            {
+                Console.WriteLine("false");
+                return false;
+            }
+            else 
+            {
+                Console.WriteLine("Ingen resultat");
+                return false;
+            }
         }
     }
 }
